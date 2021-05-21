7af4c81432d7f6b20273ecd7cf8a8ab149d2b074
ldh7228 <ldh7228@gmail.com>
2021-05-21 16:06:36 +0900

    create js file

---
+++ index.js
@@ -0,0 +1,17 @@
+function a() {
+    console.log("hello");
+} // 함수 만드는 법
+
+a()
+
+var a = 0;
+let b = 0;
+const c = 0;
+
+function asdf() {
+    a = 10;
+    b = 10;
+}
+
+asdf();
+console.log(a,b,c);
