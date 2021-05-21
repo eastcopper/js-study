40a94d0ea20edc9ad23d42cfea8548b066888746
ldh7228 <ldh7228@gmail.com>
2021-05-21 16:19:10 +0900

    update button

--- index.js
+++ index.js
@@ -15,3 +15,18 @@ function asdf() {
 
 asdf();
 console.log(a,b,c);
+
+function warning() {
+    if(confirm("확인하시겠습니까?")) {
+        alert("확인되셨습니다.")
+    }
+    else {
+        alert("취소되셨습니다.")
+    }
+}
+
+const button = document.getElementById("button");
+
+function buttonNone() {
+    button.style.backgroundColor = "black";
+}
